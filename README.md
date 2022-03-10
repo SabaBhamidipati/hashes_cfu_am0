03d5e69564a21affa87753cb7216dda22e6de7cd
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-03-09 21:08:46 -0500

    Code for nested hashes

--- README.md
+++ README.md
@@ -20,3 +20,4 @@ Based on what you know about Git workflow, make commits as you see fit.
 After you've commited and pushed your work back up to your copy of this repo, provide the repo link in the Google Submission Form.
 
 📌 Add the link to the Hash Syntax Refactor Gist you completed here:
+https://gist.github.com/SabaBhamidipati/78f63b422a9ad6aec41492f0fd8fae3d
--- hashes_1.rb
+++ hashes_1.rb
@@ -1,31 +1,36 @@
 
-Activity:
+#Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
+# file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  "apples": 23,
+  "grapes": 507,
+  "eggs": 48
+}
 
-# Write code that prints all of the 'keys' of the foods variable 
+
+# Write code that prints all of the 'keys' of the foods variable
 # you created above:
-# YOUR CODE HERE
+p foods.keys
 
-# Write code that prints all of the 'values' of the foods variable 
+# Write code that prints all of the 'values' of the foods variable
 # you created above:
-# YOUR CODE HERE
+p foods.values
 
-# Write code that prints the value of the second food of the foods variable 
+# Write code that prints the value of the second food of the foods variable
 # you created above:
-# YOUR CODE HERE
+p foods[:grapes]
 
-# Write code that adds a food to the foods hash. 
+
+# Write code that adds a food to the foods hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+foods["oranges"] = 4
 
-
+p foods
 #-------------------
 # Part 2: Email
 #-------------------
@@ -36,17 +41,24 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+email = {
+  "recipient": "saba.bhamidipati@gmail.com",
+  "subject": "Where is the proposal?",
+  "sender": "James@hotmail.com",
+  "attachments": 5,
+  "time_stamp": "2:40 a.m."
+}
 
 # Write code that prints your email hash to the terminal.
+p email
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+p email.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+p email.values
 
 
 #-------------------
@@ -74,7 +86,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -88,12 +100,38 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+
+email = [
+  {
+    "recipient": "saba.bhamidipati@gmail.com",
+    "subject": "Where is the proposal?",
+    "sender": "James@hotmail.com",
+    "attachments": 5,
+    "time_stamp": "2:40 a.m."
+  },
+  {
+    "recipient": "elena.bhamidipati@gmail.com",
+    "subject": "Meeting Time",
+    "sender": "Sam@aol.com",
+    "attachments": 2,
+    "time_stamp": "4:40 a.m."
+  },
+  {
+    "recipient": "saba.bhamidipati@gmail.com",
+    "subject": "Dinner Plans",
+    "sender": "Twitter@hotmail.com",
+    "attachments": 0,
+    "time_stamp": "2:40 p.m."
+  }
+]
+
+p email
